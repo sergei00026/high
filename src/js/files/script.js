73da5f4f41fa8c0cea58163049e23840eecb2376
sergei00026 <sergei57fx@mail.ru>
2023-10-18 10:07:58 +0300

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,19 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+// Підключення списку активних модулів
+
+const anchors = document.querySelectorAll('.link')
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		const blockID = anchor.getAttribute('href').substr(1)
+		if (anchor.closest('.menu-open')) {
+			anchor.closest('.menu-open').classList.remove('menu-open')
+		}
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+}
